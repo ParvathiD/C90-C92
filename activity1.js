1bb51a2c04b99162d02f2dbbeb17b387c06e4362
ParvathiD <82469728+ParvathiD@users.noreply.github.com>
2021-09-12 18:03:56 -0500

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,17 @@
+score = 0;
+
+function UpdateScore(){
+
+    score = score + 1;
+    document.getElementById("score").innerHTML = "Score: " + score;
+}
+
+function SaveScore(){
+
+    localStorage.setItem("score", score);
+}
+
+function NextPage(){
+
+    window.location = "activity_2.html"
+}
